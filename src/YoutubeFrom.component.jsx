26aef6ed65f5d94ecb7cb0890a8cc6e10f3acbd0
vivek11101997 <vivek11101997@gmail.com>
2021-09-09 15:21:11 +0530

    done with intro

--- src/YoutubeFrom.component.jsx
+++ src/YoutubeFrom.component.jsx
@@ -1,30 +1,19 @@
 import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
 import React from "react";
 import * as Yup from "yup";
+import { useState } from "react";
 
 const YoutubeFrom = () => {
+  const [formvalue, setformValue] = useState(null);
   const initialValues = { name: "", email: "", channel: "", phNumbers: [""] };
+  const saveValue = {
+    name: "vivek",
+    email: "v@gmail.com",
+    channel: "",
+    phNumbers: [""],
+  };
   const onSubmit = (values) => {};
-  // const validate = (values) => {
-  //   let errors = {};
-  //   if (!values.name) {
-  //     errors.name = "name is required";
-  //   }
 
-  //   if (!values.email) {
-  //     errors.email = "email is required";
-  //   } else if (
-  //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-  //   ) {
-  //     errors.email = "Invalid email format";
-  //   }
-
-  //   if (!values.channel) {
-  //     errors.channel = "channel is required";
-  //   }
-
-  //   return errors;
-  // }
   var validationSchema = Yup.object({
     name: Yup.string().required("Required"),
     email: Yup.string().email("invalid Email Format").required("Required"),
@@ -34,54 +23,67 @@ const YoutubeFrom = () => {
   //console.log(formik.touched);
   return (
     <Formik
-      initialValues={initialValues}
+      initialValues={formvalue || initialValues}
       onSubmit={onSubmit}
       validationSchema={validationSchema}
+      validateOnMount
+      enableReinitialize
     >
-      <Form>
-        <label htmlFor="name">Name</label>
-        <Field type="text" id="name" name="name" />
-        <ErrorMessage name="name" />
+      {(formik) => {
+        //console.log(formik);
+        return (
+          <Form>
+            <label htmlFor="name">Name</label>
+            <Field type="text" id="name" name="name" />
+            <ErrorMessage name="name" />
 
-        <label htmlFor="name">E-mail</label>
-        <Field type="text" id="email" name="email" />
-        <ErrorMessage name="email" />
+            <label htmlFor="name">E-mail</label>
+            <Field type="text" id="email" name="email" />
+            <ErrorMessage name="email" />
 
-        <label htmlFor="name">Channel</label>
-        <Field type="text" id="channel" name="channel" />
-        <ErrorMessage name="channel" />
+            <label htmlFor="name">Channel</label>
+            <Field type="text" id="channel" name="channel" />
+            <ErrorMessage name="channel" />
 
-        <div>
-          <label>List of Phone number</label>
-          <FieldArray name="phNumbers">
-            {(FieldArrayProps) => {
-              const { push, remove, form } = FieldArrayProps;
-              const { values } = form;
-              const { phNumbers } = values;
-              // {
-              //   !phNumbers && <div className="loading">Loading...</div>;
-              // }
-              return (
-                <div>
-                  {phNumbers.map((_, index) => (
-                    <div key={index}>
-                      <Field type="text" name={`phNumbers[${index}]`} />
-                      <button type="button" onClick={() => remove(index)}>
-                        -
-                      </button>
-                      <button type="button" onClick={() => push(" ")}>
-                        +
-                      </button>
+            <div>
+              <label>List of Phone number</label>
+              <FieldArray name="phNumbers">
+                {(FieldArrayProps) => {
+                  const { push, remove, form } = FieldArrayProps;
+                  const { values } = form;
+                  const { phNumbers } = values;
+                  // {
+                  //   !phNumbers && <div className="loading">Loading...</div>;
+                  // }
+                  return (
+                    <div>
+                      {phNumbers.map((_, index) => (
+                        <div key={index}>
+                          <Field type="text" name={`phNumbers[${index}]`} />
+                          <button type="button" onClick={() => remove(index)}>
+                            -
+                          </button>
+                          <button type="button" onClick={() => push(" ")}>
+                            +
+                          </button>
+                        </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
-              );
-            }}
-          </FieldArray>
-        </div>
+                  );
+                }}
+              </FieldArray>
+            </div>
 
-        <button type="submit">Submit</button>
-      </Form>
+            <button type="button" onClick={() => setformValue(saveValue)}>
+              Load Saved Data
+            </button>
+
+            <button type="submit" disabled={!formik.isValid}>
+              Submit
+            </button>
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
